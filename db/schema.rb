737f7422fe2d69744d1ef1a26ed862ad050eda10
fumiaki49 <me.fk39@gmail.com>
2019-09-26 20:53:19 +0900

    create posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_31_230833) do
+ActiveRecord::Schema.define(version: 2019_08_31_235630) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "song", default: "", null: false
+    t.string "singer", default: "", null: false
+    t.text "lyric", null: false
+    t.text "comment", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
