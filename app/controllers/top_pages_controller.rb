e13ab519bf12581c0c7531cb36af32d592ddd539
fumiaki49 <me.fk39@gmail.com>
2019-10-10 18:57:56 +0900

    トップページで投稿された情報を表示

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,11 +1,11 @@
 class TopPagesController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.includes(:user)
   end
 
   private
 
   def posts_params
-    params.require(:post).permit(:id, :song, :lyric, :comment).merge(user_id: current_user.id, )
+    params.require(:post).permit(:id, :song, :lyric, :comment).merge(user_id: current_user.id)
   end
 end
