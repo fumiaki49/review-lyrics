e49510c64f9b9943185a0070d2cdceff49781c2f
fumiaki49 <me.fk39@gmail.com>
2019-10-24 18:17:00 +0900

    create delete function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :ensurer_corrent_user, only: [:destroy]
 
   def new
     @post = Post.new
@@ -9,6 +10,13 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     if @post.save!
       redirect_to top_pages_path, notice: '投稿が完了しました。'
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      redirect_to my_post_user_path(current_user.id)
+      flash[:notice] = "投稿を削除しました"
     end
   end
 
@@ -22,5 +30,12 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:id, :song, :singer, :lyric, :comment).merge(user_id: current_user.id)
+  end
+
+  def ensurer_corrent_user
+    @post = Post.find_by(id: params[:id])
+    if @post.user_id == current_user.id
+      @post.destroy
+    end
   end
 end
