37b3088aa9da1cd23d08addfdf609e441d534e64
fumiaki49 <me.fk39@gmail.com>
2019-10-27 17:46:45 +0900

    ビューの微調整

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,7 +1,7 @@
 class TopPagesController < ApplicationController
   def index
     @posts = Post.all.includes(:user)
-    @posts = Post.page(params[:page]).per(7).order(created_at: :desc)
+    @posts = Post.page(params[:page]).per(10).order(created_at: :desc)
   end
 
   private
