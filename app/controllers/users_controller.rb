762aec650900032684bc2f49be6125682b4140be
fumiaki49 <me.fk39@gmail.com>
2019-11-18 18:30:26 +0900

    update my_post.html.haml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def my_post
     @posts = Post.where(user_id: current_user.id)
+    @my_posts = Post.page(params[:user_id]).per(9).order(created_at: :desc)
   end
 
   def my_favorit
